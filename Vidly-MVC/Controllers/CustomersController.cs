6c75a1d3c162060c1e2da6f10e4f0ad7497881fb
krish-nair <krish.nair101@gmail.com>
2018-07-09 22:25:49 +1200

    Add Details action and View in Customer

--- Vidly-MVC/Controllers/CustomersController.cs
+++ Vidly-MVC/Controllers/CustomersController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,12 +16,20 @@ namespace Vidly_MVC.Controllers
             return View(customers);
         }
 
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+        }
+
         private IEnumerable<Customer> GetCustomers()
         {
             return new List<Customer>
             {
                 new Customer {Id = 1, Name = "Adam Moore"},
-                new Customer {Id = 2, Name = "Gordon Ramsay"},
+                new Customer {Id = 2, Name = "Gordon Ramsay"}
             };
         }
     }
