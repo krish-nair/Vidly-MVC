71d0ac2fa11aa09dec4516a60690787834530240
krish-nair <krish.nair101@gmail.com>
2018-07-09 21:49:11 +1200

    Add Customer Controller and View

---
+++ Vidly-MVC/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly_MVC.Models;
+
+namespace Vidly_MVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Adam Moore"},
+                new Customer {Id = 2, Name = "Gordon Ramsay"},
+            };
+        }
+    }
+}
