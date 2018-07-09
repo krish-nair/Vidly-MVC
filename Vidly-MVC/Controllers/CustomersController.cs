4ac309868e3cccebee5586752426780b1b299b09
krish-nair <krish.nair101@gmail.com>
2018-07-10 00:08:16 +1200

    Update MembershipType

--- Vidly-MVC/Controllers/CustomersController.cs
+++ Vidly-MVC/Controllers/CustomersController.cs
@@ -9,28 +9,31 @@ namespace Vidly_MVC.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customer.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customer.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "Adam Moore"},
-                new Customer {Id = 2, Name = "Gordon Ramsay"}
-            };
         }
     }
 }
--- Vidly-MVC/Models/MembershipType.cs
+++ Vidly-MVC/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Web;
@@ -9,6 +10,8 @@ namespace Vidly_MVC.Models
     public class MembershipType
     {
         public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; } 
