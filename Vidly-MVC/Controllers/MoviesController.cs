98e40d2c9b6fe2a8bef311bceb7f618986a26a3d
krish-nair <krish.nair101@gmail.com>
2018-07-09 22:04:25 +1200

    Add Movie Controller and View

---
+++ Vidly-MVC/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Vidly_MVC.Models;
+
+namespace Vidly_MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Transformer"},
+                new Movie {Id = 2, Name = "Team5"}
+            };
+        }
+    }
+}
