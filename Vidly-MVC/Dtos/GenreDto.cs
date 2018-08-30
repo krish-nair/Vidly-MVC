ad6ba19ac09b8e541283e4699deb151f84670612
krish-nair <krish.nair101@gmail.com>
2018-08-30 13:41:58 +1200

    Datatables-remove record from movie tbl

--- Vidly-MVC/Dtos/GenreDto.cs
+++ Vidly-MVC/Dtos/GenreDto.cs
@@ -7,8 +7,7 @@ namespace Vidly_MVC.Dtos
 {
     public class GenreDto
     {
-        public int Id { get; set; }
-
+        public byte Id { get; set; }
         public string Name { get; set; }
     }
 }
