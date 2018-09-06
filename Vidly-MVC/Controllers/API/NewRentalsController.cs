323963e9476223ca960f73bb7cd9add0ad4a6e57
krish-nair <krish.nair101@gmail.com>
2018-09-06 13:13:04 +1200

    Add Rental class to models

---
+++ Vidly-MVC/Controllers/API/NewRentalsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly_MVC.Dtos;
+
+namespace Vidly_MVC.Controllers.API
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
