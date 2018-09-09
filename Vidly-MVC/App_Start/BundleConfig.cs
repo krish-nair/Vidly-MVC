07ae340c7970971b7a356ecaa24f51eb9283ea16
krish-nair <krish.nair101@gmail.com>
2018-09-09 18:31:15 +1200

    Add toastr

--- Vidly-MVC/App_Start/BundleConfig.cs
+++ Vidly-MVC/App_Start/BundleConfig.cs
@@ -15,7 +15,8 @@ namespace Vidly_MVC
                         "~/Scripts/respond.js",
                         "~/Scripts/Datatables/jquery.datatables.js",
                         "~/Scripts/Datatables/datatables.bootstrap.js",
-                        "~/Scripts/typeahead.bundle.js"
+                        "~/Scripts/typeahead.bundle.js",
+                        "~/Scripts/toastr.js"
                       ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -30,6 +31,7 @@ namespace Vidly_MVC
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
+                      "~/Content/toastr.css",
                       "~/Content/site.css"));
         }
     }
